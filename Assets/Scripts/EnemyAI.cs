dc2d8dfc49fa9b7c6ded8f5e443cb457631d37c4
xenaapb <xena.apb@gmail.com>
2023-07-03 11:51:27 +0100

    starting ai

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 10.0f;
+    public Vector2 player;
+    public Vector2 enemy;
+   // public Camera cam;
+    void Start()
+    {
+        
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
