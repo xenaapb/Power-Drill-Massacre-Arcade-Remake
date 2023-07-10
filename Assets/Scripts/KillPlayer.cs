ab62671a1aa7faa5a4fb49e633d5dbd0c0f70ab8
xenaapb <xena.apb@gmail.com>
2023-07-10 11:47:47 +0100

    editing killplayer script further

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -6,23 +6,41 @@ using UnityEngine.SceneManagement;
 public class KillPlayer : MonoBehaviour
 {
     public int Respawn;
+
+    bool shouldKill = false;
+    float killTimer = 1f;
+
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = GameObject.Find("Player").GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (shouldKill)
+        {
+            if (killTimer > 0)
+            {
+                anim.SetBool("ShouldKill", true);
+
+                killTimer = killTimer - Time.deltaTime;
+            }
+            else
+            {
+                SceneManager.LoadScene(Respawn);
+            }
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(Respawn);
+            shouldKill = true;
         }
     }
 }
